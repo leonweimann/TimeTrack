5a12a5e21b98d3453ddd2cdd2e52864868fc913b
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-09-29 20:15:58 +0200

    Initial Commit

---
+++ TimeTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTrack
+//
+//  Created by Leon Weimann on 29.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTrack/TimeTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackApp.swift
+//  TimeTrack
+//
+//  Created by Leon Weimann on 29.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
