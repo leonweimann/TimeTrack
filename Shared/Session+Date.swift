34e46762c177fd409c0133a3a5f5ea4a1fa19d72
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-09-29 22:55:51 +0200

    Updated Session to support date logic

---
+++ Shared/Session+Date.swift
@@ -0,0 +1,27 @@
+//
+//  Session+Date.swift
+//  TimeTrack
+//
+//  Created by Leon Weimann on 29.09.24.
+//
+
+import EventKit
+
+extension Session {
+    var startAtDate: Date {
+        TimeInterval(startAt).toTodayDate
+    }
+    
+    var endAtDate: Date {
+        guard let endAt else { return startAtDate }
+        return TimeInterval(endAt).toTodayDate
+    }
+}
+
+extension Session {
+    func eventWithDate(_ date: Date, store: EKEventStore, calendar: EKCalendar? = nil) throws -> EKEvent {
+        guard isCurrent else { throw SessionError.notFinished }
+        let newEvent = EKEvent(self, eventStore: store, calendar: calendar)
+        return newEvent
+    }
+}
