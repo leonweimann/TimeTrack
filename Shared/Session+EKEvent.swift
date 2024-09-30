8707b31dc19e7c8e5710b0c119cb857fcaf0450a
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-09-30 15:53:34 +0200

    Added Session init via EKEvent

--- Shared/Session+EKEvent.swift
+++ Shared/Session+EKEvent.swift
@@ -8,6 +8,31 @@
 import EventKit
 
 extension Session {
+    // TODO: SessionType is not handled!
+    init(event: EKEvent) throws {
+        guard
+            let string = event.url?.absoluteString,
+            string.hasPrefix("session-")
+        else {
+            throw SessionError.creationFailure(.notSessionEvent)
+        }
+        
+        guard let id = string.split(separator: "-").last else {
+            throw SessionError.creationFailure(.noID)
+        }
+        
+        guard let index = event.title.firstIndex(of: " ") else {
+            throw SessionError.creationFailure(.noName)
+        }
+        let name = event.title.suffix(from: index)
+        
+        let endDate = (event.endDate == event.startDate) ? nil : event.endDate
+
+        self.init(id: String(id), type: .personal, name: String(name), startDate: event.startDate, endDate: endDate)
+    }
+}
+
+extension Session {
     func event(store: EKEventStore, calendar: EKCalendar? = nil) throws -> EKEvent {
         guard isCurrent else { throw SessionError.notFinished }
         let newEvent = EKEvent(self, eventStore: store, calendar: calendar)
