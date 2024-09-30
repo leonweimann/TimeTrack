f12f399910d45ac98e98a0c51bb957562488d2c4
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-10-01 01:45:27 +0200

    Introduced alreadyFinished case to SessionError
    Preparing for mutating endNow method in Session

--- Shared/SessionError.swift
+++ Shared/SessionError.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum SessionError: Error {
     case notFinished
+    case alreadyFinished
     case creationFailure(CreationFailure)
 }
 
@@ -17,6 +18,8 @@ extension SessionError: LocalizedError {
         switch self {
         case .notFinished:
             return NSLocalizedString("Session is not finished yet.", comment: "Not finished")
+        case .alreadyFinished:
+            return NSLocalizedString("Session is already finished yet.", comment: "Already finished")
         case .creationFailure(let failure):
             return NSLocalizedString("Session could not be created due to \(failure.rawValue)", comment: "Creation failed")
         }
