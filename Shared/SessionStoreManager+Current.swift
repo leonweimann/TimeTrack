6044f92840e7ead3866108225b920d6ee27990e7
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-10-01 03:16:14 +0200

    Added capabilities regarding currents to SessionStoreManager
    Added:
    - currents property, which filters and sorts the sessions array appropriate.
    - createSession: Creates a new current session (doesn't saves it to datastore)
    - updateSession: Updates an already existing Session in the sessions array, by validating first.
    - finishSession: Marks a current Session as finished by configuring it appropriate.

---
+++ Shared/SessionStoreManager+Current.swift
@@ -0,0 +1,31 @@
+//
+//  SessionStoreManager+Current.swift
+//  TimeTrack
+//
+//  Created by Leon Weimann on 01.10.24.
+//
+
+import Foundation
+
+extension SessionStoreManager {
+    var currents: [Session] {
+        sessions
+            .filter { $0.isCurrent }
+            .sorted { $1.startDate < $0.startDate }
+    }
+    
+    func createSession(_ session: Session) throws {
+        guard session.isCurrent else { throw SessionError.alreadyFinished }
+        sessions.append(session)
+    }
+    
+    func updateSession(_ session: Session) throws {
+        guard session.isCurrent else { throw SessionError.alreadyFinished }
+        guard let index = sessions.firstIndex(of: session) else { throw SessionError.notFound }
+        sessions[index] = session
+    }
+    
+    func finishSession(_ session: Session) throws {
+        guard session.isCurrent else { throw SessionError.alreadyFinished }
+    }
+}
